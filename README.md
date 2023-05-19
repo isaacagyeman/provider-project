f693110f4db8866fc791446f580c52121bd38f5f
unknown <isaacagyeman437@gmail.com>
2023-05-19 23:22:32 +0100

    fixed missing page error

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # providerproject
 
-A new Flutter project.
+This is a project I built to test my knowledge and try my hands on the famous state management package (provider)
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This project is a mixture of sub-projects built into one:
+- Todo page
+- Food page
+- Jokes page
+- Track Ip address
 
-A few resources to get you started if this is your first Flutter project:
+ All the above mentioned pages were implemented using third party apis and the provider package for state management.
+Except for the Track IP address page that is yet to be completed.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:providerproject/Screens/LoginPage.dart';
+import 'package:providerproject/Screens/MainPage.dart';
+// import 'package:providerproject/Screens/LoginPage.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -15,24 +16,23 @@ class _SplashState extends State<Splash> {
     super.initState();
     Timer(
         const Duration(seconds: 7),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: ((context) => const LoginPage()))));
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: ((context) => Mainpage()))));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.blue
-        ),
-        child:  const Center(
-          child:  Text("HOLA MUNDO",
-          textAlign: TextAlign.center, 
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight:FontWeight.w500,
-          ),
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: const Center(
+          child: Text(
+            "HOLA MUNDO",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       ),
